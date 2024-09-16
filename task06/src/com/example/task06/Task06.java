96a9f0a59ec44a93ba9264bd970e3b863a905125
blankec <Komba-rus05@mail.ru>
2024-09-16 12:53:21 +0500

    Кочетков Вадим. Задание 06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,24 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int sumNumbers = x + y;
+        int count = 0;
+        while(sumNumbers != 0){
+            count += 1;
+            sumNumbers /= 10;
+        }
 
-        return 0;
+        if (count == 0) return 1;
+        return count;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
